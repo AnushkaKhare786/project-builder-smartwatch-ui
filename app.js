f56912fb1007a2d43431212cd78ce3e53571ce81
Anushka Khare <67369620+AnushkaKhare786@users.noreply.github.com>
2022-04-18 18:37:22 +0530

    Anushka Khare Batch 106

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-var today = new Date();
-var day = today.getDay();
-var hour = today.getHours();
-var min = today.getMinutes();
-var daylist = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-document.getElementById("time").innerHTML=hour+":"+min;
-document.getElementById("day").innerHTML=daylist[day];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var setTime = setInterval(showtime, 1000);
+
+
+function showtime()
+{
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var day = today.getDay();
+
+
+    if(hours<10)
+    {
+        hours= `0${hours}`;
+    }
+
+    if(minutes<10)
+    {
+        minutes=`0${minutes}`;
+
+    }
+
+       toptime.innerHTML=hours+":"+ minutes;
+       middletime.innerHTML= hours+":"+minutes;
+       daydisplay.innerHTML= days[day];
+}
+
+      setInterval(time,1000);
